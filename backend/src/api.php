850df2f9852c8ff3ce06d9d5b51127516302ff00
jocerdas25 <georgedevcode@users.noreply.github.com>
2023-11-03 17:16:10 -0600

    Trabajando en el API del backend

---
+++ backend/src/api.php
@@ -0,0 +1,19 @@
+<?php
+    use \Psr\Http\Message\ServerRequestInterface as Request;
+    use \Psr\Http\Message\ResponseInterface as Response;
+
+    require '../vendor/autoload.php';
+
+    $app = new \Slim\App;
+
+    $app->get("/hello/", function (Request $request, Response $response, array $args){
+         
+        // $name = $args['name'];
+
+        $response->getBody()->write("Hello, World!");
+
+        return $response;
+    });
+
+    $app->run();
+?>
