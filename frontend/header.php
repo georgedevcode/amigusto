7458246a1aeac3e85251c38c51843e8b3458fcd0
jocerdas25 <georgedevcode@users.noreply.github.com>
2023-11-16 18:30:58 -0600

    trabajando en pagina contactenos

--- frontend/header.php
+++ frontend/header.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>"A mi gusto" | Restaurante</title>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,600,700" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
 <header>
     <div class="contenedor">
       <nav class="menu .navbar">
@@ -18,10 +30,10 @@
               <a href="menu.php" id="btn-menu">Menú</a>
             </li>
             <li>
-              <a href="#" id="btn-galeria">Servicios</a>
+              <a href="#" id="btn-servicios">Servicios</a>
             </li>
             <li>
-              <a href="#" id='btn-ubicacion'>Contactenos</a>
+              <a href="contactenos.php" id='btn-contactenos'>Contactenos</a>
             </li>
             <li>
               <a href="#" id='btn-registro'>Registrarte</a>
