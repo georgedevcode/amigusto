598ce8b68daf3e34e07861d1a117c84913208465
Jorge Cerdas <jcerdas25@gmail.com>
2023-11-22 13:11:14 -0600

    Cambie de ubicacion los 404.html y 500.html dentro de la carpeta frontend

--- frontend/menu.php
+++ frontend/menu.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>"A mi gusto" | Restaurante</title>
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,600,700" rel="stylesheet">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/estilos.css">
-</head>
-<body>
 <?php
     include "header.php"
 ?>
@@ -74,8 +62,3 @@
 <?php
     include "footer.php"
 ?>
-  <script src="libs/jquery-3.2.1.min.js"></script>
-  <script src="js/efectos.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-</body>
-</html>
