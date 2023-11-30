39f9b2843cf82dfab8d225707e4dd73f39deb25e
Jorge Cerdas <jcerdas25@gmail.com>
2023-11-30 16:23:20 -0600

    Trabajando en la API para el menu

--- backend/src/public/index.php
+++ backend/src/public/index.php
@@ -2,11 +2,49 @@
     use \Psr\Http\Message\ServerRequestInterface as Request;
     use \Psr\Http\Message\ResponseInterface as Response;
 
+    include '../../vendor/adodb/adodb-php/adodb.inc.php';
     require '../../vendor/autoload.php';
+    
+    //Variables de conexion de MySQL
+    $driver = 'mysqli';
+    $database = "amigustodb";
+    $host = "localhost:8080";
+    $user = "root";
+    $password = "mysql";
 
+    function dbConexion(){
+
+        //Conexion a la base de datos
+        $db = newAdoConnection($GLOBALS['driver']);
+
+        $db->connect($GLOBALS['host'], $GLOBALS['user'], $GLOBALS['password'], $GLOBALS['database']);
+
+        if($db->isConnected())
+            echo "<script>Console.log('Connected to DB')</script>";
+
+        return $db;
+    }
     $app = new \Slim\App;
 
-    $app->get("/hello/", function (Request $request, Response $response, array $args){
+    $app->get('/getmenu', function(Request $request, Response $response, array $args){
+        
+        $conx = dbConexion();
+
+        $query = "SELECT * FROM 'tmenu'";
+
+        $conx->SetFetchMode(ADODB_FETCH_ASSOC);
+
+        $sqlData = $conx->GetAll($query);
+
+        $response->getBody()->write(json_encode($sqlData));
+
+        return $response;
+
+    });
+
+   
+
+    $app->get("/hello", function (Request $request, Response $response, array $args){
          
         // $name = $args['name'];
 
--- frontend/menu.php
+++ frontend/menu.php
@@ -8,50 +8,58 @@
             <h3 class="titulo" id='platillos'>Menú</h3>
             <div class="contenedor-menu">
               <div class="contenedor-menu2">
-                <article>
+                <article id="platillos">
                   <p class="categoria">Especiales:</p>
                   <div class="platillo">
                     <p class="nombre">Sandwich jamon y queso</p>
                     <p class="precio">₡3500</p>
                     <p class="description">Pan a escoger,jamon de pavo, queso americano, lechuga, tomate y salsas.</p>
+                    <p class="tiempo"></p>
                   </div>
                   <div class="platillo">
                     <p class="nombre">Empanadas:</p>
                     <p class="precio">₡1400</p>
                     <p class="description">Queso, Frijol, Carne, Pollo, Chicharon</p>
+                    <p class="tiempo"></p>
                   </div>
                   <div class="platillo">
                     <p class="nombre">Enchiladas:</p>
                     <p class="precio">₡1000</p>
                     <p class="description">Papa, Carne, Pollo, frjol con queso.</p>
+                    <p class="tiempo"></p>
                   </div>
                   <div class="platillo">
                     <p class="nombre">Postres</p>
                     <p class="precio">₡2500</p>
                     <p class="description">Tres leches, pie de limon, Queque de fresa, Tamal de elote.</p>
+                    <p class="tiempo"></p>
                   </div>
                 </article>
-                <article>
+                <article id="bebidas">
                   <p class="categoria">Cafés:</p>
                   <div class="platillo">
                     <p class="nombre">Café Negro</p>
                     <p class="precio">₡1000</p>
                     <p class="description">Café de altura.</p>
+                    <p class="tiempo"></p>
                   </div>
                   <div class="platillo">
                     <p class="nombre">Capuchino</p>
                     <p class="precio">₡1500</p>
                     <p class="description">Café de altura, leche.</p>
+                    <p class="tiempo"></p>
                   </div>
                   <div class="platillo">
                     <p class="nombre">Mocaccino</p>
                     <p class="precio">₡1800</p>
                     <p class="description">Café de altura, leche, chocolate</p>
+                    <p class="tiempo"></p>
                   </div>
                   <div class="platillo">
                     <p class="nombre">Expresso Gelato</p>
                     <p class="precio">₡2100</p>
                     <p class="description">Café expresso, leche, crema irlandesa, helado a escoger.</p>
+                    <p class="tiempo"></p>
                   </div>
                 </article>
               </div>
