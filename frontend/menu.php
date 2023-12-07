e7b81526b37bf875dc21491a5472092e4e08a84c
Jorge Cerdas <jcerdas25@gmail.com>
2023-12-07 10:36:13 -0600

    Organizando el codigo

--- frontend/menu.php
+++ frontend/menu.php
@@ -1,72 +1,25 @@
 <?php
-    include "header.php"
+  include "header.php"
 ?>
-<section class="main container-sm">
-        <!-- Fin de Acerda de -->
-        <section class="menu container-sm">
-          <div class="contenedor">
-            <h3 class="titulo" id='platillos'>Menú</h3>
-            <div class="contenedor-menu">
-              <div class="contenedor-menu2">
-                <article id="platillos-menu">
-                  <p class="categoria">Especiales:</p>
-                  <!-- <div class="platillo">
-                    <p class="nombre">Sandwich jamon y queso</p>
-                    <p class="precio">₡3500</p>
-                    <p class="description">Pan a escoger,jamon de pavo, queso americano, lechuga, tomate y salsas.</p>
-                    <p class="tiempo"></p>
-                  </div>
-                  <div class="platillo">
-                    <p class="nombre">Empanadas:</p>
-                    <p class="precio">₡1400</p>
-                    <p class="description">Queso, Frijol, Carne, Pollo, Chicharon</p>
-                    <p class="tiempo"></p>
-                  </div>
-                  <div class="platillo">
-                    <p class="nombre">Enchiladas:</p>
-                    <p class="precio">₡1000</p>
-                    <p class="description">Papa, Carne, Pollo, frjol con queso.</p>
-                    <p class="tiempo"></p>
-                  </div>
-                  <div class="platillo">
-                    <p class="nombre">Postres</p>
-                    <p class="precio">₡2500</p>
-                    <p class="description">Tres leches, pie de limon, Queque de fresa, Tamal de elote.</p>
-                    <p class="tiempo"></p>
-                  </div> -->
-                </article>
-                <article id="bebidas-menu">
-                  <p class="categoria">Cafés:</p>
-                  <!-- <div class="platillo">
-                    <p class="nombre">Café Negro</p>
-                    <p class="precio">₡1000</p>
-                    <p class="description">Café de altura.</p>
-                    <p class="tiempo"></p>
-                  </div>
-                  <div class="platillo">
-                    <p class="nombre">Capuchino</p>
-                    <p class="precio">₡1500</p>
-                    <p class="description">Café de altura, leche.</p>
-                    <p class="tiempo"></p>
-                  </div>
-                  <div class="platillo">
-                    <p class="nombre">Mocaccino</p>
-                    <p class="precio">₡1800</p>
-                    <p class="description">Café de altura, leche, chocolate</p>
-                    <p class="tiempo"></p>
-                  </div>
-                  <div class="platillo">
-                    <p class="nombre">Expresso Gelato</p>
-                    <p class="precio">₡2100</p>
-                    <p class="description">Café expresso, leche, crema irlandesa, helado a escoger.</p>
-                    <p class="tiempo"></p>
-                  </div> -->
-                </article>
-              </div>
-            </div>
+  <section class="main container-sm">
+    <!-- Fin de Acerda de -->
+    <section class="menu container-sm">
+      <div class="contenedor">
+        <h3 class="titulo" id='platillos'>Menú</h3>
+        <div class="contenedor-menu">
+          <div class="contenedor-menu2">
+            <article id="platillos-menu">
+              <p class="categoria">Especiales:</p>
+              <!-- Aqui van los platillos del menu -->
+            </article>
+            <article id="bebidas-menu">
+              <p class="categoria">Cafés:</p>
+              <!-- Aqui van las bebidas -->
+            </article>
           </div>
-        </section>
-
-        <?php
-    include "footer.php";
-  ?>       
+        </div>
+      </div>
+  </section>
+<?php
+  include "footer.php";
+?>       
