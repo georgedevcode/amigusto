f8a361bbbac02b91979862f4e7fa9c8f2449e6b8
Jorge Cerdas <jcerdas25@gmail.com>
2023-12-09 10:34:01 -0600

    Agregando contenido a la pagina de servicios

--- frontend/servicios.php
+++ frontend/servicios.php
@@ -1,14 +1,21 @@
 <?php include "header.php"?>
 <body>
-    <section>
-        <h2>Nuestros Servicios</h2>
-        <p>Descubre una amplia variedad de cafés y deliciosos postres.</p>
-        <a href="menu.php">Ver Menú</a>
-    </section>
-    <section>
-        <h2>Horario de Atención</h2>
-        <p>Lunes a Viernes: 8:00 AM - 9:00 PM<br>Sábado y Domingo: 11:00 AM - 8:00 PM</p>
-    </section>
+    <div class="contenedor">
+        <section>
+            <h2>Nuestros Servicios</h2>
+            <ul>
+                <li>Servicio de catering para su eventos especiales.</li>
+                <li>Salon privado para eventos ejecutivos, baby showers, cumplea_nos y mas...;</li>
+                <li>Degustaciones de platillos con chef privado.</li>
+            </ul>
+            <p>Descubre una amplia variedad de cafés y deliciosos postres.</p>
+            <a href="menu.php">Ver Menú</a>
+        </section>
+        <section>
+            <h2>Horario de Atención</h2>
+            <p>Lunes a Viernes: 8:00 AM - 9:00 PM<br>Sábado y Domingo: 11:00 AM - 8:00 PM</p>
+        </section>
+    </div>
     <?php
     include "footer.php";
   ?>       
