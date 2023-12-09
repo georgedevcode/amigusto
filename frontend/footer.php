8a029466b5e2727673103b4cdbe7887c626c57fb
Jorge Cerdas <jcerdas25@gmail.com>
2023-12-09 11:00:43 -0600

    Cambimos en el footer.php para solo cargar el menu.js cuando estamos en la pagina menu.php

--- frontend/footer.php
+++ frontend/footer.php
@@ -5,7 +5,19 @@
   </footer>
   <script src="libs/jquery-3.2.1.min.js"></script>
   <script src="js/efectos.js"></script>
-  <script src="js/menu.js"></script>
+
+  <?php
+
+    $url=basename($_SERVER["PHP_SELF"]); 
+    $url=explode(".",$url); 
+
+    if ($url[0] == 'menu') {
+    
+        echo '<script src="js/menu.js"></script>';
+
+   };
+
+  ?>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
