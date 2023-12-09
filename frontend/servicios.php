058700b62f51b7998a738a2784febc353423d1d3
Jorge Cerdas <jcerdas25@gmail.com>
2023-12-09 10:25:54 -0600

    Arreglando el menu para que sea responsivo

--- frontend/servicios.php
+++ frontend/servicios.php
@@ -1,16 +1,13 @@
 <?php include "header.php"?>
 <body>
-
     <section>
         <h2>Nuestros Servicios</h2>
         <p>Descubre una amplia variedad de cafés y deliciosos postres.</p>
         <a href="menu.php">Ver Menú</a>
     </section>
-
     <section>
         <h2>Horario de Atención</h2>
         <p>Lunes a Viernes: 8:00 AM - 9:00 PM<br>Sábado y Domingo: 11:00 AM - 8:00 PM</p>
-        <p>Prueba</p>
     </section>
     <?php
     include "footer.php";
